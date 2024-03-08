a08726ac7c3f96c4ae371ada752d8a3db899991a
fadymaher24 <fady.maher2024@gmail.com>
2024-03-09 00:55:55 +0200

    fix bug

--- src/main.py
+++ src/main.py
@@ -24,7 +24,7 @@ resnet = InceptionResnetV1(pretrained='vggface2').eval().to(device)
 def collate_fn(x):
     return x[0]
 
-dataset = datasets.ImageFolder('../data/test_images')
+dataset = datasets.ImageFolder('/home/fadymaher/git-fedora/Face-Recognition-Project/data/test_images')
 dataset.idx_to_class = {i:c for c, i in dataset.class_to_idx.items()}
 loader = DataLoader(dataset, collate_fn=collate_fn, num_workers=workers)
 
